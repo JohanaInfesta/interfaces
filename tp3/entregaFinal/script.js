20d7d074fc56dee4696319123baf3aab8c72563a
Johana Infesta <johana.infesta@gmail.com>
2019-09-20 14:38:27 -0300

    tp3 finalizado

--- tp3/entregaFinal/script.js
+++ tp3/entregaFinal/script.js
@@ -3,7 +3,8 @@ let obj = document.getElementById("personaje");
 obj.addEventListener("mousemove", function(event){
 var x = window.event.x;
 var y = window.event.y;
-obj.style.left = x + "px";
+obj.style.left = x - 10 + "px";
 obj.style.top = y + "px";
+obj.style.right = (x - obj.style.left)+ "px";
 console.log(x, y)
 });
