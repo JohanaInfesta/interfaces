21f375fe67ac01d8bc49297302053f1ca8625fe7
Johana Infesta <johana.infesta@gmail.com>
2019-09-25 15:02:32 -0300

    falta aplicar los obstaculos y un score

--- tp3/juego/script.js
+++ tp3/juego/script.js
@@ -1,20 +1,32 @@
-let space = false;
-let canvas = document.getElementById("pj");
-// let ctx = canvas.getContext("2d");
+let avatar = document.getElementById("pj");
+let pj = {y : 280, vy : 0, gravedad :2 , salto : 80, vymax : 9, saltar : false}
 
-
-window.addEventListener("keyup", function(event){
+document.addEventListener("keydown", function(event){
   if(event.keyCode == 32){
-    space = false;
-    canvas.classList.remove("personaje-saltar");
-    canvas.classList.add("personaje");
+    pj.saltar = true;
+    pj.vy = pj.salto;
+    gravedad();
   }
 });
-window.addEventListener("keydown", function(event){
+
+function gravedad(){
+  if(pj.saltar == true){
+    if( pj.y < 280){
+      pj.saltar = false;
+      pj.vy = 0;
+      pj.y = 280;
+      avatar.style.top = pj.y + "px";
+    }else{
+      pj.vy -= pj.gravedad;
+      //pj.y -= pj.vy
+      avatar.style.top = (pj.y - pj.vy) + "px";
+    }
+  }
+}
+window.addEventListener("keyup", function(event){
   if(event.keyCode == 32){
-    space = true;
-    console.log("space");
-    canvas.classList.add("personaje-saltar");
-    canvas.classList.remove("personaje");
+    saltar = false;
+    avatar.style.top = 280 + "px";
+    // return space;
   }
 });
