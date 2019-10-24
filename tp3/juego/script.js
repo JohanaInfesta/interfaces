fca8dacf2f0e6bc638608ed62a5727ba0c0ab0ad
Johana Infesta <johana.infesta@gmail.com>
2019-10-23 22:58:36 -0300

    arreglando juego

--- tp3/juego/script.js
+++ tp3/juego/script.js
@@ -1,5 +1,4 @@
 let avatar = document.getElementById("pj");
-let pj = {y : 290, vy : 0, gravedad :2 , salto : 150, vymax : 9, saltar : false}
 let suelo = 290;
 
 let btnJugar = document.getElementById("jugar");
@@ -8,39 +7,40 @@ btnJugar.addEventListener("click", jugar);
 function jugar(){//empezar juego
 
   document.onkeydown = function(){saltarAvatar()};
-  document.onkeyup = function(){bajarAvatar()};
   setInterval(function(){
     requestAnimationFrame(enemigos);
-  }, Math.random()*5000+1);
-  // score();
-setInterval('score()', 500);
+  }, Math.random()*6000+1);
+  setInterval('score()', 500);
 }
 
+let pj = {posicion : avatar.offsetHeight, y : 290, vy : 0, gravedad :2 , salto : 150, vymax : 9, saltar : false}
 
 function saltarAvatar(){
   if(event.keyCode == 32){
     pj.saltar = true;
     pj.vy = pj.salto;
     gravedad();
-    // requestAnimationFrame(gravedad);
   }
 }
 function gravedad(){
-  if(pj.saltar == true){
+  if(pj.saltar == true && nivel.muerto == false){
     if( pj.y < suelo){
       pj.saltar = false;
       pj.vy = 0;
       pj.y = suelo;
       avatar.style.top = pj.y + "px";
     }else{
-      pj.vy -= pj.gravedad;
+      pj.vy += pj.gravedad;
       avatar.style.top = (pj.y -= pj.vy) + "px";
+      setTimeout(function(){
+        bajarAvatar()
+      }, 600);
     }
   }
 }
 
 function bajarAvatar(){
-  if(event.keyCode == 32){
+  if(pj.saltar == true){
     pj.saltar = false;
     avatar.style.top = suelo + "px";
   }
@@ -64,23 +64,24 @@ let puntuacion = {inicio : 0, fin : 100};
 function score(){
   var pts = document.getElementById("puntos");
 
-    if(nivel.muerto == false && puntuacion.inicio >= puntuacion.fin){
-      console.log("win")
-      puntuacion.inicio = 0;
-      //ganaste
-    }else if(nivel.muerto == true){
-      console.log("muerto");
-      document.getElementById("loser").style.display = "initial";
-    }else{
-      puntuacion.inicio ++;
-      pts.innerHTML = puntuacion.inicio;
-    }
+  if(nivel.muerto == false && puntuacion.inicio >= puntuacion.fin){
+    puntuacion.inicio = 0;
+    document.getElementById("win").style.display = "initial";
+    //ganaste
+
+  }else if(nivel.muerto == true){
+    document.getElementById("loser").style.display = "initial";
+    //perdiste
+  }else{
+    puntuacion.inicio ++;
+    pts.innerHTML = puntuacion.inicio;
+  }
   // },100);
-return puntuacion.inicio;
+  return puntuacion.inicio;
 }
 
 function colision(){
-  if((pjEnemigo.x >= 100 ) && (pjEnemigo.x <= 120 )){
+  if((pjEnemigo.x >= 70 ) && (pjEnemigo.x <= 90)){
     if(pj.saltar == true){
       nivel.muerto = false;
     }else{
